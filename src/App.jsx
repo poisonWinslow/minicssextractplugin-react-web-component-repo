3aec4df593a8fac36cfa4c54ab8b03d6c28342fe
poisonWinslow <42157052+poisonWinslow@users.noreply.github.com>
2021-07-12 11:09:53 -0400

    Update App.jsx
    Moved React rendering into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import * as styles from "./App.module.scss";
 
 const App = () => {
-  return (
-    <div id="App">
-      <h1 className={styles.someStyle}>This should be red</h1>
-      <p>This should be green</p>
-    </div>
-  );
+  const shadowRoot = document.querySelector("custom-web-component")?.shadowRoot;
+  const reactRoot = shadowRoot?.getElementById("react-root");
+
+  if (reactRoot) {
+    ReactDOM.render(
+      <React.StrictMode>
+        <div id="App">
+          <h1 className={styles.someStyle}>This should be red</h1>
+          <p>This should be green</p>
+        </div>
+      </React.StrictMode>,
+      reactRoot
+    );
+  }
 };
 
 export default App;
